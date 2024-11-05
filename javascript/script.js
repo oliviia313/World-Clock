0d08327a4a4957e868d3085943406cd99c5c2f60
Valery Naumova <oliviia313@gmail.com>
2024-11-04 23:58:36 -0500

    js for a single location added

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,36 @@
-let cities = document.querySelector("#cities");
-function showTime(e) {
-  if (e.target.value.length > 0) {
-    let dateTime = moment()
-      .tz(e.target.value)
-      .format("dddd, MMMM D, YYYY hh:mm A");
-    alert(`It is currently ${dateTime} in ${e.target.value}`);
+// let cities = document.querySelector("#cities");
+// function showTime(e) {
+//   if (e.target.value.length > 0) {
+//     let dateTime = moment()
+//       .tz(e.target.value)
+//       .format("dddd, MMMM D, YYYY hh:mm A");
+//     alert(`It is currently ${dateTime} in ${e.target.value}`);
+//   }
+// }
+// cities.addEventListener("change", showTime);
+function updateTime() {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  if (losAngelesElement) {
+    let losAngelesDateElement = losAngelesElement.querySelector(".date");
+    let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+    let losAngelesTime = moment().tz("America/Los_Angeles");
+
+    losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM	Do YYYY");
+    losAngelesTimeElement.innerHTML = losAngelesTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+
+  let parisElement = document.querySelector("#paris");
+  if (parisElement) {
+    let parisDateElement = parisElement.querySelector(".date");
+    let parisTimeElement = parisElement.querySelector(".time");
+    let parisTime = moment().tz("Europe/Paris");
+
+    parisDateElement.innerHTML = parisTime.format("MMMM	Do YYYY");
+    parisTimeElement.innerHTML = parisTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
   }
 }
-cities.addEventListener("change", showTime);
+setInterval(updateTime, 1000);
